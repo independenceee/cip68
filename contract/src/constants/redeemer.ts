1ec38827dbe77e75a09251449aca7f81ac397e1c
Nguyễn Khánh <108068667+independenceee@users.noreply.github.com>
2024-08-21 17:55:51 +0700

    develop: write smart contract write testcase using lucid

---
+++ contract/src/constants/redeemer.ts
@@ -0,0 +1,13 @@
+// lucid cardano begin
+import { Constr, Data } from "lucid-cardano";
+
+
+export const mintRedeemer = Data.to(new Constr(0, []));
+export const burnRedeemer = Data.to(new Constr(1, []));
+export const updateRedeemer = Data.to(new Constr(0, []));
+// lucid cardano end
+
+// mesh cardano begin
+
+
+// mesh cardano endclear
